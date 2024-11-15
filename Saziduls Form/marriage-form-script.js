cae05a10c90eb252f47cc1f6314b86361d204317
Sazidul014 <sazidullaskar618@gmail.com>
2024-11-15 18:41:15 +0530

    Delete Saziduls Form directory

--- Saziduls Form/marriage-form-script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("marriageForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevents the form from submitting the default way
-    alert("Your application for Ekram's marriage form has been submitted successfully!");
-    // Code to handle form data submission can be added here
-});
